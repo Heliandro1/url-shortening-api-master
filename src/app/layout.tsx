3e49fd26f2f4d344a779306385dc2f0820c71776
ACC <64018848+Heliandro1@users.noreply.github.com>
2024-07-02 14:27:17 +0100

    update: updating meta content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,26 +6,33 @@ import { ReactQueryProvider } from "@/utils/providers/query-provider";
 const poppins = Poppins({ weight: ["500", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Frontend Mentor | Shortly URL shortening API Challenge",
-  description: "Frontend Mentor Challenge",
+	title: "Frontend Mentor | Shortly URL shortening API Challenge",
+	description: "Frontend Mentor Challenge",
+	openGraph: {
+		title: "Caxala URL Shortener",
+		description: "A simple and quick way to short your links",
+		images: "/images/og-image.png",
+		type: "website",
+		url: "https://url-shortening-api-master-steel.vercel.app/",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <link
-        rel="shortcut icon"
-        href="/images/favicon.png"
-        type="image/png"
-        sizes="32x32"
-      />
-      <body className={poppins.className}>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<link
+				rel="shortcut icon"
+				href="/images/favicon.png"
+				type="image/png"
+				sizes="32x32"
+			/>
+			<body className={poppins.className}>
+				<ReactQueryProvider>{children}</ReactQueryProvider>
+			</body>
+		</html>
+	);
 }
